fd840eb020ae6df26666c3a3726f473de1d9b2fa
Wessel Huising <wessel.huising@Wessels-MacBook-Pro.local>
2024-08-19 09:37:38 +0200

    add ufo plugin

---
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,18 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = { "kevinhwang91/promise-async" },
+  --    event = 'VeryLazy',   -- You can make it lazy-loaded via VeryLazy, but comment out if thing doesn't work
+  init = function()
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+  end,
+  config = function()
+    require("ufo").setup({
+      -- your config goes here
+      -- open_fold_hl_timeout = ...,
+      -- provider_selector = function(bufnr, filetype)
+      --  ...
+      -- end,
+    })
+  end,
+}
