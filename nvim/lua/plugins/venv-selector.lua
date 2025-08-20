04c83a7f47c6e9ee83d600e76d80a21ffb04d318
Wessel Huising <wessel@MacBook-Pro-van-Wessel.local>
2025-08-20 10:13:15 +0200

    iupdate venv selector

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -8,28 +8,10 @@ return {
   },
   lazy = false,
   branch = "regexp", -- This is the regexp branch, use this for the new version
-  config = function()
-    require("venv-selector").setup()
-  end,
   keys = {
     { ",v", "<cmd>VenvSelect<cr>" },
   },
+  opts = {
+    -- Your settings go here
+  },
 }
--- return {
---   "linux-cultist/venv-selector.nvim",
---   branch = "regexp", -- Use this branch for the new version
---   cmd = "VenvSelect",
---   enabled = function()
---     return LazyVim.has("telescope.nvim")
---   end,
---   opts = {
---     settings = {
---       options = {
---         notify_user_on_venv_activation = true,
---       },
---     },
---   },
---   --  Call config for python files and load the cached venv automatically
---   ft = "python",
---   keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
--- }
